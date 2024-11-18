2d64b03eaa167a91ead38bb640a6cc0fedc28397
jagadishchakma <jagadishchakma401@gmail.com>
2024-11-18 17:40:27 +0600

    update 3.0

--- tfbFoundation/bodhidharaAPI/models.py
+++ tfbFoundation/bodhidharaAPI/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # bodhidhara news insert model
 class BodhidharaNews(models.Model):
@@ -10,5 +11,28 @@ class BodhidharaNews(models.Model):
     fb_post_react = models.JSONField(null=True, blank=True)
     views = models.JSONField(default=list)
     created_at = models.DateTimeField(auto_now_add=True)
+
+# bodhidhara news comment model
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comments')
+    news = models.ForeignKey(BodhidharaNews, on_delete=models.CASCADE, related_name='comments')
+    comment = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+# bodhidhdara news reply1 model
+class Reply1(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='replies1')
+    comment = models.ForeignKey(Comment, on_delete=models.CASCADE, related_name='replies1')
+    reply1 = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+# bodhidhara news reply2
+class Reply2(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='replies2')
+    reply1 = models.ForeignKey(Reply1, on_delete=models.CASCADE, related_name='replies2')
+    reply2 = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
 
 
--- tfbFoundation/bodhidharaAPI/serializers.py
+++ tfbFoundation/bodhidharaAPI/serializers.py
@@ -1,7 +1,26 @@
 from rest_framework import serializers
-from .models import BodhidharaNews
+from .models import BodhidharaNews, Comment, Reply1, Reply2
 #BodhidharaNews Serializers
 class BodhadharaNewsSerializers(serializers.ModelSerializer):
     class Meta:
         model = BodhidharaNews
         fields = '__all__'
+
+#Comment Serializer
+class CommentSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = Comment 
+        fields = '__all__'
+
+#Reply1 Serializer
+class Reply1Serializers(serializers.ModelSerializer):
+    class Meta:
+        model = Reply1
+        fields = '__all__'
+
+#Reply2 Serializer
+class Reply2Serializers(serializers.ModelSerializer):
+    class Meta:
+        model = Reply2 
+        fields = '__all__'
+        
